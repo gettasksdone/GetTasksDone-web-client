57aea673c8ae9d0385d6e704b04b6e7e1e582b25
Matías Alonso <93829504+MataloDeEiras@users.noreply.github.com>
2023-11-02 15:24:46 +0100

    Remade Dockerfile and devcontainer.json.

---
+++ .devcontainer/startup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+/etc/init.d/xrdp stop
+/etc/init.d/xrdp start
--- build.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd $1
-
-/home/developer/flutter/bin/flutter build web
-
-cp -a build/web/. /var/www/html
