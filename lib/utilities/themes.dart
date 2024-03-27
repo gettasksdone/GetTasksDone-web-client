dafa86f142cd2e9b92839bea1ba14730e7685a3b
matalo <matiasalonsoeiras@gmail.com>
2024-03-27 11:28:12 +0000

    Quick fix

--- lib/utilities/themes.dart
+++ lib/utilities/themes.dart
@@ -6,7 +6,6 @@ class AppTheme {
     colorScheme: ColorScheme.fromSeed(
       seedColor: const Color.fromRGBO(51, 84, 230, 1),
       onSecondary: Colors.white,
-      onBackground: Colors.white,
       tertiary: Colors.grey.darken(45),
       secondary: Colors.grey.darken(55),
       onSurface: Colors.white.darken(40),
