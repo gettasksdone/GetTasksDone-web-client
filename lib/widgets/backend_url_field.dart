61f1c0f11d90c838cc20c9732b8e84e58ae4b94a
Matías Alonso <93829504+MataloDeEiras@users.noreply.github.com>
2024-04-28 19:56:39 +0000

    Arreglado el listado de opciones

--- lib/widgets/backend_url_field.dart
+++ lib/widgets/backend_url_field.dart
@@ -1,6 +1,7 @@
 import 'package:gtd_client/logic/backend_configuration.dart';
 import 'package:gtd_client/widgets/custom_form_field.dart';
 import 'package:gtd_client/utilities/validators.dart';
+import 'package:gtd_client/utilities/constants.dart';
 import 'package:flutter/material.dart';
 
 class BackendUrlField extends StatefulWidget {
@@ -16,36 +17,62 @@ class _BackendUrlFieldState extends State<BackendUrlField> {
 
   @override
   Widget build(BuildContext context) {
-    return Autocomplete(
-      initialValue: TextEditingValue(text: _backendConfiguration.url),
-      fieldViewBuilder: (context, controller, focusNode, onFieldSubmitted) {
-        return CustomFormField(
-          focusNode: focusNode,
-          controller: controller,
-          label: 'URL del backend',
-          hintText: 'url del backend',
-          validator: (String? input) => notEmptyValidator(
-            input,
-            () {
-              if (input != null) {
-                setState(() {
-                  _backendConfiguration.set(input);
-                });
+    return LayoutBuilder(
+      builder: (context, constraints) => Autocomplete(
+        initialValue: TextEditingValue(text: _backendConfiguration.url),
+        fieldViewBuilder: (context, controller, focusNode, onFieldSubmitted) {
+          return CustomFormField(
+            focusNode: focusNode,
+            controller: controller,
+            label: 'URL del backend',
+            hintText: 'url del backend',
+            validator: (String? input) => notEmptyValidator(
+              input,
+              () {
+                if (input != null) {
+                  setState(() {
+                    _backendConfiguration.set(input);
+                  });
 
-                onFieldSubmitted();
-              }
-            },
+                  onFieldSubmitted();
+                }
+              },
+            ),
+          );
+        },
+        optionsBuilder: (textEditingValue) {
+          if (textEditingValue.text.isEmpty) {
+            return const Iterable<String>.empty();
+          }
+
+          return _backendConfiguration.urls.where((String option) =>
+              option.contains(textEditingValue.text.toLowerCase()));
+        },
+        optionsViewBuilder: (context, onSelected, options) => Align(
+          alignment: Alignment.topLeft,
+          child: Material(
+            borderRadius: roundedCorners,
+            child: SizedBox(
+              height: 52.0 * options.length,
+              width: constraints.biggest.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  for (final String option in options)
+                    InkWell(
+                      borderRadius: roundedCorners,
+                      onTap: () => onSelected(option),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(option),
+                      ),
+                    ),
+                ],
+              ),
+            ),
           ),
-        );
-      },
-      optionsBuilder: (textEditingValue) {
-        if (textEditingValue.text.isEmpty) {
-          return const Iterable<String>.empty();
-        }
-
-        return _backendConfiguration.urls.where((String option) =>
-            option.contains(textEditingValue.text.toLowerCase()));
-      },
+        ),
+      ),
     );
   }
 }
