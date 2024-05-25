24ba89b60d35225034fb9de7cfa6a5a1291667f9
Matías Alonso <93829504+MataloDeEiras@users.noreply.github.com>
2024-05-25 18:25:27 +0000

    Limpiado del repositorio

--- lib/utilities/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'dart:math';
-
-final Random _random = Random();
-
-Color getRandomColor() {
-  return Colors.primaries[_random.nextInt(Colors.primaries.length)];
-}
--- lib/utilities/pair.dart
+++
@@ -1,6 +0,0 @@
-class Pair<T, G> {
-  final T first;
-  final G last;
-
-  const Pair({required this.first, required this.last});
-}
